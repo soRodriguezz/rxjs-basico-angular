864a1fe5f64cb8128264cdf28ce7d0207bb9d5b0
sorodriguezz <sebastian.rodriguez19@outlook.com>
2021-11-22 20:52:12 -0300

    forkjoin

---
+++ src/combinacion/04-combineLatest.ts
@@ -0,0 +1,28 @@
+import { combineLatest, fromEvent, map, pluck } from "rxjs";
+
+// const keyup$ = fromEvent(document, 'keyup');
+// const click$ = fromEvent(document, 'click');
+
+// combineLatest(
+//     keyup$.pipe(pluck('type')),
+//     click$.pipe(pluck('type'))
+// ).subscribe(console.log);
+
+const input1 = document.createElement('input');
+const input2 = document.createElement('input');
+
+input1.placeholder = 'email@gmail.com';
+input2.placeholder = '**********';
+
+input2.type = 'password';
+
+document.querySelector('body').append(input1, input2);
+
+const getInputStream = (elem: HTMLInputElement) => fromEvent<KeyboardEvent>(elem, 'keyup').pipe(
+        pluck<any>('target', 'value'),
+    )
+
+combineLatest(
+    getInputStream(input1),
+    getInputStream(input2)
+).subscribe(console.log);

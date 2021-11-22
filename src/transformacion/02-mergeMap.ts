5027f78ea8dfe0485d98a4a6e1ae4a5a09ae4edd
sorodriguezz <sebastian.rodriguez19@outlook.com>
2021-11-22 15:03:47 -0300

    MergeMap

---
+++ src/transformacion/02-mergeMap.ts
@@ -0,0 +1,25 @@
+import { fromEvent, interval, map, mergeMap, of, take, takeUntil } from "rxjs";
+
+
+const letras$ = of('a', 'b', 'c');
+
+
+// letras$.pipe(
+//     mergeMap( (letra) => interval(1000).pipe(
+//         map(i => letra + i),
+//         take(3),
+//     ))
+// ).subscribe({
+//     next: (letra) => console.log('next:', letra),
+//     complete: () => console.log('complete')
+// });
+
+const mouseDown$ = fromEvent(document, 'mousedown');
+const mouseUp$ = fromEvent(document, 'mouseup');
+const interval$ = interval();
+
+mouseDown$.pipe(
+    mergeMap(() => interval$.pipe(
+        takeUntil(mouseUp$)
+    ))
+).subscribe(console.log);
